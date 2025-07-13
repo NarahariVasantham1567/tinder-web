c67731b921fcb69c2cbc682476d00849a1522da7
NarahariVasantham1567 <naraharivasantham1567@gmail.com>
2025-07-13 20:45:02 +0530

    redirect to login if not authenticated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,37 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import axios from 'axios';
+import { BASE_URL } from '../utils/constants';
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser } from '../utils/userSlice';
+import { useEffect } from 'react';
 
 export default function HomePage() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const user = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    if (user) return;
+    try {
+      const res = await axios.get(`${BASE_URL}/profile/view`, {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate('/login');
+      }
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <Navbar />
