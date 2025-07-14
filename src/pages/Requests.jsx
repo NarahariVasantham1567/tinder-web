710de9ce98da31f8eb209aa7980f4306c3a2b384
NarahariVasantham1567 <naraharivasantham1567@gmail.com>
2025-07-14 18:49:23 +0530

    feat: reaquests & connections page added

--- src/pages/Requests.jsx
+++ src/pages/Requests.jsx
@@ -1,12 +1,25 @@
 import axios from 'axios';
 import { BASE_URL } from '../utils/constants';
 import { useDispatch, useSelector } from 'react-redux';
-import { addRequest } from '../utils/requestSlice';
+import { addRequest, removeRequest } from '../utils/requestSlice';
 import { useEffect } from 'react';
 
 export default function Requests() {
   const requests = useSelector((store) => store.requests);
   const dispatch = useDispatch();
+
+  const reviewRequest = async (status, _id) => {
+    try {
+      const res = await axios.post(
+        `${BASE_URL}/request/review/${status}/${_id}`,
+        {},
+        { withCredentials: true }
+      );
+      dispatch(removeRequest(_id));
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   const fetchRequests = async () => {
     try {
@@ -58,8 +71,18 @@ export default function Requests() {
               <p>{about}</p>
             </div>
             <div>
-              <button className='btn btn-primary mx-2'>Reject</button>
-              <button className='btn btn-secondary mx-2'>Accept</button>
+              <button
+                className='btn btn-primary mx-2'
+                onClick={() => reviewRequest('rejected', request._id)}
+              >
+                Reject
+              </button>
+              <button
+                className='btn btn-secondary mx-2'
+                onClick={() => reviewRequest('accepted', request._id)}
+              >
+                Accept
+              </button>
             </div>
           </div>
         );
